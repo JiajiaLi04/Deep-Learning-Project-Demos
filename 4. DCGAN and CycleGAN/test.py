914f800cdd9882fbda6c5d5b1446e151b08c27d1
Jiajia Li <jiajiali@host-210330.dhcp.egr.msu.edu>
2020-01-18 16:10:17 -0500

    add new project

---
+++ 4. DCGAN and CycleGAN/test.py
@@ -0,0 +1,17 @@
+
+# Numpy & Scipy imports
+import numpy as np
+import scipy
+import scipy.misc
+
+# Torch imports
+import torch
+import torch.nn as nn
+
+
+a=torch.randn((3,3))
+print(a)
+b = a.mul(a)
+b = 1/2*3
+print(b)
+
